a46ad0d58573bb231ee1689aadd530eab1863e8a
Your Name <vverma4313@gmail.com>
2020-12-11 23:46:03 +0530

    Added styles, added reset and pause functionality

--- src/Countdown.js
+++ src/Countdown.js
@@ -1,65 +1,91 @@
 import React, { Component } from "react";
-
+import "./App.css";
 class Countdown extends Component {
   constructor(props) {
     super(props);
     this.state = {
       timerOn: false,
-      timerTime: "mm",
-      seconds: 0,
+      timerTime: "Minutes",
+      seconds: "00",
       timerStart: 0,
     };
   }
 
-  handleClick = () => {
+  handleStart = () => {
     this.setState({
       timerOn: true,
       timerTime: this.state.timerTime,
       timerStart: this.state.timerTime,
       seconds: this.state.seconds,
     });
-    this.timeIterval = setInterval(() => {
+    this.timeInterval = setInterval(() => {
+      const seconds = this.state.seconds;
+      const timerTime = this.state.timerTime;
+      if (seconds === 0 && timerTime > 0) {
+        this.setState({
+          timerTime: timerTime - 1,
+          seconds: 60,
+        });
+      }
       const newTime = this.state.seconds - 1;
       if (newTime >= 0) {
         this.setState({
           seconds: newTime,
         });
       } else {
-        clearInterval(this.timeIterval);
+        clearInterval(this.timeInterval);
         this.setState({ timerOn: false });
       }
     }, 1000);
-    const seconds = this.state.seconds;
-    const timerTime = this.state.timerTime;
-    if (seconds === 0) {
-      this.setState({
-        timerTime: timerTime - 1,
-        seconds: 59,
-      });
-    }
   };
 
   handleTimerChange = (event) => {
     this.setState({
       timerTime: event.target.value,
+      seconds: 0,
     });
   };
+
+  handleStop = () => {
+    clearInterval(this.timeInterval);
+    this.setState({ timerOn: false });
+  };
+
+  handleReset = () => {
+    if (this.state.timerOn === false) {
+      this.setState({
+        timerTime: "00",
+        seconds: "00",
+      });
+    }
+  };
   render() {
-    const { timerTime, timerOn, seconds } = this.state;
+    const { timerTime, timerStart, timerOn, seconds } = this.state;
     return (
-      <div>
-        Countdown Timer
+      <div className="main">
+        <h2>Countdown Timer</h2>
         <input
           onChange={this.handleTimerChange}
           type="number"
-          placeholder="time in minutes"
+          placeholder="Enter time in minutes..."
           required
         />
-        <div>
+        <div className="container">
           <h1>
             {timerTime} : {seconds}
           </h1>
-          <button onClick={this.handleClick}>Start</button>
+          {timerOn === false &&
+            (timerStart === 0 || timerTime === timerStart) && (
+              <button onClick={this.handleStart}>Start</button>
+            )}
+          {timerOn === true && <button onClick={this.handleStop}>Stop</button>}
+          <button onClick={this.handleReset}>Reset</button>
+          {timerOn === false &&
+            timerStart !== 0 &&
+            timerStart !== timerTime &&
+            timerTime !== 0 && (
+              <button onClick={this.handleStart}>Resume</button>
+            )}
         </div>
       </div>
     );
